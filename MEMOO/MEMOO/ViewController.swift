c9f29d5c1b49f09469ed2567de82678b17b52101
Aleksey Dolinin <allDolinin@gmail.com>
2021-07-28 02:56:50 +0300

    3

---
+++ MEMOO/MEMOO/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MEMOO
+//
+//  Created by Aleksey Dolinin on 28.07.2021.
+//  Copyright © 2021 Aleksey Dolinin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
