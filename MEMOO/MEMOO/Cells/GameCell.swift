28136157eb274fbc947a771dd4fc9219400c18c7
Aleksey Dolinin <allDolinin@gmail.com>
2021-08-10 10:03:55 +0300

    2

--- MEMOO/MEMOO/Cells/GameCell.swift
+++ MEMOO/MEMOO/Cells/GameCell.swift
@@ -3,12 +3,10 @@ import UIKit
 class GameCell: UICollectionViewCell {
 
     @IBOutlet weak var imageContent: UIImageView!
-    @IBOutlet weak var imageNameContent: UIImageView!
     @IBOutlet weak var back: UIView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        back.layer.cornerRadius = 16
     }
 
     func setCell(content: String) {
@@ -16,19 +14,14 @@ class GameCell: UICollectionViewCell {
         switch content {
         case "fruit":
             imageContent.image = UIImage(named: "fruit_im")
-            imageNameContent.image = UIImage(named: "fruit_l")
         case "animal_":
             imageContent.image = UIImage(named: "animal_im")
-            imageNameContent.image = UIImage(named: "animals_l")
         case "emoji_":
             imageContent.image = UIImage(named: "emoji_im")
-            imageNameContent.image = UIImage(named: "emoji_l")
         case "dinoCad_":
             imageContent.image = UIImage(named: "dino_im")
-            imageNameContent.image = UIImage(named: "dino_l")
         case "monster_":
             imageContent.image = UIImage(named: "monster_im")
-            imageNameContent.image = UIImage(named: "monster_l")
 
         default:
             break
