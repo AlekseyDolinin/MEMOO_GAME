93f0d354a70170449b03fa476ed1f5672020b74d
allDolinin <allDolinin@gmail.com>
2021-11-16 09:44:47 +0300

    Верстка

--- MEMOO/MEMOO/Controllers/StartViewController/StartViewController.swift
+++ MEMOO/MEMOO/Controllers/StartViewController/StartViewController.swift
@@ -11,7 +11,7 @@ class StartViewController: UIViewController, GADFullScreenContentDelegate {
     static var nameGame: String!
     static var unlockAllGame: Bool!
     
-    var listGame = ["fruit_", "emoji_", "color_", "animal_", "dinosaur_", "monster_", "alfred_", "animall_", "color_", "mandala_", "ninja_",  "sport_", "summer_", "toy_", "word_", "wordd_", "dog_", "toyy_", "vegetable_", "animalll_", "letter_"]
+    var listGame = ["fruit_", "emoji_", "animal_", "dinosaur_", "monster_", "alfred_", "animall_", "mandala_", "ninja_",  "sport_", "summer_", "toy_", "word_", "wordd_", "dog_", "toyy_", "vegetable_", "space_", "letter_", "origami_", "letterr_", "animalll_", "farm_", "flower_", "fauna_", "flag_"]
     var currentIndex = 0
     var rewardedAd: GADRewardedAd?
     let valuePeriodWithooutADVInSeconds = 50 /// 3 часа - 10800
@@ -32,19 +32,19 @@ class StartViewController: UIViewController, GADFullScreenContentDelegate {
     
     ///
     func chekLockedGame() {
-        let listGameAdv = ["animal_", "dinosaur_", "monster_"]
-        for nameGame in listGameAdv {
-            let dateSeeADV: Date? = UserDefaults.standard.object(forKey: nameGame + "date") as? Date
-            print(">>>>>>>>>>>>\(nameGame) - \(String(describing: dateSeeADV))")
-            if dateSeeADV != nil {
-                /// проверка как давно был просмотр рекламы
-                let secondsCount = Int(Date().timeIntervalSince1970 - dateSeeADV!.timeIntervalSince1970)
-                print(secondsCount)
-                if secondsCount >= valuePeriodWithooutADVInSeconds {
-                    UserDefaults.standard.set(nil, forKey: nameGame + "date")
-                }
-            }
-        }
+//        let listGameAdv = ["animal_", "dinosaur_", "monster_"]
+//        for nameGame in listGameAdv {
+//            let dateSeeADV: Date? = UserDefaults.standard.object(forKey: nameGame + "date") as? Date
+//            print(">>>>>>>>>>>>\(nameGame) - \(String(describing: dateSeeADV))")
+//            if dateSeeADV != nil {
+//                /// проверка как давно был просмотр рекламы
+//                let secondsCount = Int(Date().timeIntervalSince1970 - dateSeeADV!.timeIntervalSince1970)
+//                print(secondsCount)
+//                if secondsCount >= valuePeriodWithooutADVInSeconds {
+//                    UserDefaults.standard.set(nil, forKey: nameGame + "date")
+//                }
+//            }
+//        }
     }
     
     ///
--- MEMOO/MEMOO/Views/GameView.swift
+++ MEMOO/MEMOO/Views/GameView.swift
@@ -3,22 +3,14 @@ import UIKit
 class GameView: UIView {
     
     @IBOutlet weak var cardCollection: UICollectionView!
-    @IBOutlet weak var viewForTimer: UIView!
-    @IBOutlet weak var topBar: UIView!
-    @IBOutlet weak var topConstraint: NSLayoutConstraint!
     @IBOutlet weak var menuButton: UIButton!
         
     override func awakeFromNib() {
         super.awakeFromNib()
-        topBar.transform = CGAffineTransform(translationX: 0, y: -140)
         menuButton.isUserInteractionEnabled = false
     }
     
     func showTopBar() {
-        UIView.animate(withDuration: 0.5, delay: 1, usingSpringWithDamping: 0.5, initialSpringVelocity: 5, options: .curveEaseInOut, animations: {
-            self.topBar.transform = .identity
-        }) { (true) in
-            self.menuButton.isUserInteractionEnabled = true
-        }
+        self.menuButton.isUserInteractionEnabled = true
     }
 }
